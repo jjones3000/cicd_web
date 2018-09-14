503a590bc8c0655e94494c5ff99769d5399c5181
jjones3000 <jeff@gridlevelwd.com>
2018-09-13 20:23:36 -0500

    Add files via upload

--- webserver.sh
+++ webserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash/
 sudo ssh -i /home/ec2-user/cicd_web.pem ec2-user@52.91.156.49
-sudo git clone https://github.com/jjones3000/cicd_web.git /var/www/html/
+sudo git pull https://github.com/jjones3000/cicd_web.git /var/www/html/
